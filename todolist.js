49160a1ff2618f003712b63fff22021399019908
roma36022 <koterka@mail.ru>
2023-10-21 20:14:19 +0300

    init

---
+++ todolist.js
@@ -0,0 +1,79 @@
+const dom = {
+    new : document.getElementById('new'),
+    add: document.getElementById('add'),
+    tasks: document.getElementById('tasks')
+}
+
+const tasks = [];
+
+//Отслеживаем клик по кнопке добавить Добавление задачи
+dom.add.onclick = () => {
+    const newTaskText = dom.new.value
+    if(newTaskText && isNotHaveTask(newTaskText, tasks)){
+        addTask(newTaskText, tasks)
+        dom.new.value = ""  //При добавлении задачи, мы меняем поле меняем переменную на пустую строку
+        tasksRender(tasks)
+    }
+}
+//Функция добавления задачи
+function addTask(text, list) {
+const timestamp = Date.now() // Метод позволяющий брать уникальное значение времени, которое будет являться нашим id
+const task = {
+    id: timestamp, 
+    text,
+    isComplete: false
+}
+    list.push(task)
+    console.log(tasks)
+    }
+
+
+//Функция проверки на совпадение текста с уже существующей задачей
+function isNotHaveTask(text, list) {
+    let isNotHave = true //Изначально полагаем, что функция нет (отстутсвие = правда)
+
+    list.forEach((task) => {
+        if(task.text == text){
+            alert('Задачи уже существует!')
+            dom.new.value = ""
+            isNotHave = false //Если задача есть, то хуярим ей ложь, что отсутствует 
+        }
+    })  
+    return isNotHave
+}
+
+
+// Функция вывода списка задач
+function tasksRender(list) {
+    let htmlList = ''
+
+    list.forEach((task) => {
+        const cls = task.isComplete 
+        ? "todo_task todo__task_complete" 
+        : "todo__task"  // в этом месте я вместо "__" написал "_" нихуя не работало и я час искал ошибку
+        const checked  = task.isComplete ? 'checked' : ''
+
+        const taskHtml = `
+        <div id="${task.id}" class="${cls}">
+            <label class="todo__checkbox" >
+                <input type="checkbox" ${checked}>
+                <div class = "todo__checkbox-div"></div>
+            </label>
+            <div class="todo__task-text"> ${task.text}</div>
+            <div class="todo__task-del">-</div>
+        </div>
+        `
+        htmlList = htmlList + taskHtml
+    })
+
+    dom.tasks.innerHTML = htmlList
+}
+
+dom.tasks.onclick = (event) => {
+    const target = event.target 
+    if (target.classList.contains("todo__checkbox-div")){
+        alert("This is checkbox")
+        console.log(target)
+    }
+    
+}
